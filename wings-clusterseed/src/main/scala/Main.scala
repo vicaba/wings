949affc57c134e1ccf0d6c817aa1c48c24720b9e
vicaba <vicaba@me.com>
2016-09-13 09:14:32 +0200

    utils namespace remved

--- wings-clusterseed/src/main/scala/Main.scala
+++ wings-clusterseed/src/main/scala/Main.scala
@@ -1,7 +1,5 @@
-import akka.actor.{Props, ActorSystem}
+import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
-import wings.utils.test.RandomActor
-import wings.utils.test.SuperRandomActor
 
 
 /**
@@ -20,13 +18,6 @@ object  Main {
         case "seed2" =>
           val seed2 = ActorSystem("PubSubCluster", ConfigFactory.parseString(s"akka.remote.netty.tcp.port=3001").
             withFallback(ConfigFactory.load("app")))
-        case "playpubsub" =>
-          val playpubsub = ActorSystem("PlayPubSub", ConfigFactory.load("playsystem"))
-          playpubsub.actorOf(SuperRandomActor.props(), "SuperRandomActor")
-        case "mqttpubsub" =>
-          val mqttpubsub = ActorSystem("MqttPubSub", ConfigFactory.parseString(s"akka.remote.netty.tcp.port=3003").
-            withFallback(ConfigFactory.load("playsystem")))
-          mqttpubsub.actorOf(Props(new RandomActor()))
       }
     }
   }
